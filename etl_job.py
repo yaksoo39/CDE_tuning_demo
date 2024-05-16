7dde7f63de0eaef7c3740271fa525c416058aea5
Henry Park <yaksoo39@gmail.com>
2024-05-17 07:06:52 +0900

    Update etl_job.py for customized input and output table names

--- etl_job.py
+++ etl_job.py
@@ -1,14 +1,28 @@
 from pyspark.sql import SparkSession
+import sys
 
 def stdev_rand_list(size):
-  return size*size*size
+    return size*size*size
 
-spark = SparkSession.builder.getOrCreate()
+def main(username):
+    spark = SparkSession.builder.getOrCreate()
 
-spark.udf.register("BUSY_FUNC", stdev_rand_list)
+    spark.udf.register("BUSY_FUNC", stdev_rand_list)
 
-spark.sql('''SELECT *, BUSY_FUNC(r) as r_func 
-             FROM tuning_demo_input_table 
-             ORDER BY key''')\
-     .write.mode('overwrite')\
-     .saveAsTable('tuning_demo_output_table')
+    input_table_name = f"tuning_demo_input_table_{username}"
+    output_table_name = f"tuning_demo_output_table_{username}"
+
+    spark.sql(f'''SELECT *, BUSY_FUNC(r) as r_func 
+                  FROM {input_table_name} 
+                  ORDER BY key''')\
+         .write.mode('overwrite')\
+         .saveAsTable(output_table_name)
+
+    spark.stop()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: spark_script.py <username>")
+        sys.exit(1)
+    main(sys.argv[1])
+
